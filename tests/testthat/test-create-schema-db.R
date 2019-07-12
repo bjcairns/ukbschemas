e9973078962c92f9e27f53a66e77e92dc6ad31d3
Benjamin J. Cairns <ben.cairns@ndph.ox.ac.uk>
2019-07-12 22:21:51 +0100

    Use unique tempfile()s in tests; custom Travis-CI script

--- tests/testthat/test-create-schema-db.R
+++ tests/testthat/test-create-schema-db.R
@@ -4,7 +4,7 @@ context("test-create-schema-db")
 
 skip_if_not(curl::has_internet(), "Skipping tests; no internet")
 
-test_db_file <- basename(tempfile(fileext = ".sqlite"))
+test_db_file <- function() basename(tempfile(fileext = ".sqlite"))
 test_db_path <- tempdir()
 
 
@@ -13,53 +13,63 @@ test_db_path <- tempdir()
 test_that("create_schema_db() runs without errors or warnings", {
   
   expect_error(
-    db1 <- create_schema_db(file = test_db_file, path = test_db_path),
+    db1 <- create_schema_db(file = test_db_file(), path = test_db_path),
     NA
   )
   expect_false(DBI::dbIsValid(db1))
-  tryCatch(file.remove(paste0(test_db_path, "/", test_db_file)))
   
   expect_warning(
-    db2 <- create_schema_db(file = test_db_file, path = test_db_path),
+    db2 <- create_schema_db(file = test_db_file(), path = test_db_path),
     NA
   )
   expect_false(DBI::dbIsValid(db2))
-  tryCatch(file.remove(paste0(test_db_path, "/", test_db_file)))
   
 })
 
 test_that("create_schema_db() runs silently if required", {
   expect_silent(
     db <- create_schema_db(
-      file = test_db_file,
+      file = test_db_file(),
       path = test_db_path,
       silent = TRUE
     )
   )
   expect_false(DBI::dbIsValid(db))
-  tryCatch(file.remove(paste0(test_db_path, "/", test_db_file)))
 })
 
 test_that("create_schema_db() fails on overwrite = FALSE, non-interactive", {
-  db1 <- create_schema_db(file = test_db_file, path = test_db_path)
+  
+  expect_error(
+    db1 <- create_schema_db(file = test_db_file(), path = test_db_path),
+    NA
+  )
+  
   expect_false(DBI::dbIsValid(db1))
+  
   expect_error(
     {
-      db2 <- create_schema_db(file = test_db_file, path = test_db_path)
+      db2 <- create_schema_db(file = test_db_file(), path = test_db_path)
       suppressWarnings(DBI::dbDisconnect(db2))
     },
     UKBSCHEMA_ERRORS$OVERWRITE
   )
-  tryCatch(file.remove(paste0(test_db_path, "/", test_db_file)))
+  
 })
 
 test_that("create_schema_db() fails to overwrite when db is connected", {
-  db1 <- create_schema_db(file = test_db_file, path = test_db_path)
-  db1 <- DBI::dbConnect(db1)
+  
+  expect_error(
+    {
+      db1 <- create_schema_db(file = test_db_file(), path = test_db_path)
+      db1 <- DBI::dbConnect(db1)
+    },
+    NA
+  )
+  
   expect_error(
     {
       db2 <- create_schema_db(
-        file = test_db_file, 
+        file = test_db_file(), 
         path = test_db_path,
         overwrite = TRUE
       )
@@ -67,8 +77,9 @@ test_that("create_schema_db() fails to overwrite when db is connected", {
     },
     UKBSCHEMA_ERRORS$FAILED_OVERWRITE
   )
-  DBI::dbDisconnect(db1)
-  tryCatch(file.remove(paste0(test_db_path, "/", test_db_file)))
+  
+  suppressWarnings(DBI::dbDisconnect(db1))
+  
 })
 
 test_that("create_schema_db() won't allow in-memory databases", {
